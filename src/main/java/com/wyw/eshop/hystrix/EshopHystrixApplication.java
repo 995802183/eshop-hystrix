38b50673d1b582e402b8732816d6819bf8374e60
wangyongwen <995802183@qq.com>
2019-08-09 14:43:43 +0800

    modify

---
+++ src/main/java/com/wyw/eshop/hystrix/EshopHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.wyw.eshop.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopHystrixApplication.class, args);
+    }
+
+}
